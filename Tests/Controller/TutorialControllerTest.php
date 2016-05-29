252e9b0fa04135c0430805d15abf34425b432f7f
david verdier <david verdier>
2016-05-29 11:39:46 +0200

    Try first test

---
+++ Tests/Controller/TutorialControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Discutea\DTutoBundle\Tests\Controller;
+
+use Discutea\DTutoBundle\Tests\TestBase;
+
+
+/**
+ * TutorialControllerTest
+ * 
+ * @package  DTutorialBundle
+ * @author   David Verdier <contact@discutea.com>
+ * @access   public
+ */
+class TutorialControllerTest extends TestBase
+{
+
+    public function testIndexAction()
+    {
+        $url = $this->client->getContainer()->get('router')->generate('discutea_tuto_homepage');
+        $this->tryUrlFull($url); //start test content if empty
+    }
+    
+}
